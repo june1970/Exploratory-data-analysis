d4a70255f681b63935a463aefb1125089f8fca86
grace <57066054+june1970@users.noreply.github.com>
2020-05-16 19:50:34 -0400

    Add files via upload

---
+++ Clean dataset.R
@@ -0,0 +1,28 @@
+
+
+ setwd("C:/Users/JN/Desktop/Coursera data science specification/Course 4 Exploratory data analysis/Exploratory data analysis with plot")
+ t <- read.table("household_power_consumption.txt", header=TRUE, sep=";", na.strings = "?", colClasses = c('character','character','numeric','numeric','numeric','numeric','numeric','numeric','numeric'))
+ 
+ ## Format date to Date type
+ t$Date <- as.Date(t$Date, "%d/%m/%Y")
+ 
+ ## Loading and Filtering dataset from Feb. 1, 2007 to Feb. 2, 2007
+  t <- subset(t,Date >= as.Date("2007-2-1") & Date <= as.Date("2007-2-2"))
+ 
+ ## Remove incomplete observation
+  t <- t[complete.cases(t),]
+ 
+   ## Combine Date and Time column
+   dateTime <- paste(t$Date, t$Time)
+
+   ## Name the vector
+   dateTime <- setNames(dateTime, "DateTime")
+
+ ## Remove Date and Time column
+ t <- t[ ,!(names(t) %in% c("Date","Time"))]
+
+ ## Add DateTime column
+ t <- cbind(dateTime, t)
+ 
+ ## Format dateTime Column
+ t$dateTime <- as.POSIXct(dateTime)
